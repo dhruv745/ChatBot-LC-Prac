bed43d7008ed6144b5b1ea9a0982b53ae35cc39c
Dhruv Devani <dhruv.create12@gmail.com>
2026-02-10 10:44:06 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import os
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.title("ChatBot with LangChain and Streamlit")
+
+prompt = ChatPromptTemplate.from_messages([("system", "You're a helpful assistant. Answer the Natural Language Answer with accuracy"), 
+                                          ("user", "Question:{question}")])
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+input_text = st.text_input("Enter your question:")
+output_parser = StrOutputParser()
+
+chain = prompt | llm | output_parser
+
+if st.button("Get Answer"):
+    if input_text:
+        response = chain.invoke({"question": input_text})
+        st.write("Answer:", response.content)
+    else:
+        st.write("Please enter a question.")
+
+# To run this app, use the command: streamlit run app.py
